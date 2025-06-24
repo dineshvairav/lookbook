f0b9f3a460517ec5824759e4d808e37f2935d6d3
Dinesh Vairav <dineshvairav75@gmail.com>
2025-06-24 05:47:56 +0000

    20 seconds to redirect

--- src/app/downloads/page.tsx
+++ src/app/downloads/page.tsx
@@ -52,7 +52,7 @@ function DownloadsPageContent() {
 
     const resetInactivityTimer = () => {
       clearTimeout(inactivityTimer);
-      inactivityTimer = setTimeout(redirectToShop, 12000); // 12 seconds
+      inactivityTimer = setTimeout(redirectToShop, 20000); // 20 seconds
     };
 
     const activityEvents: (keyof WindowEventMap)[] = [
